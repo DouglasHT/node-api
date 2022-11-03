8a00b8c906007029cda7054a93bb0eb6a2fe7d85
DouglasHT <douglastripoloni@brasilcard.net>
2022-11-03 10:21:00 -0300

    feat: create and test

--- src/routes/heroRoutes.js
+++ src/routes/heroRoutes.js
@@ -1,45 +1,76 @@
-const Joi = require('joi')
-const BaseRoute = require('./base/baseRoute')
-
+const Joi = require("joi");
+const BaseRoute = require("./base/baseRoute");
+const failAction = (request, headers, error) => {
+  throw error;
+};
 class HeroRoutes extends BaseRoute {
-    constructor(db){
-        super()
-        this.db = db
-    }
+  constructor(db) {
+    super();
+    this.db = db;
+  }
 
-    list(){
-        return {
-            path: '/herois',
-            method: 'GET',
-            config: {
-                validate: {
-                    failAction: (request, headers, error) => {
-                        throw error
-                    },
-                    query: {
-                        skip: Joi.number().integer().default(0),
-                        limit: Joi.number().integer().default(10),
-                        nome: Joi.string().min(3).max(50)
-                    }
-                }
-            },
-            handler: (request, headers) => {
-                try{
-                    const {skip, limit, nome} = request.query
+  list() {
+    return {
+      path: "/herois",
+      method: "GET",
+      config: {
+        validate: {
+          failAction: (request, headers, error) => {
+            throw error;
+          },
+          query: {
+            skip: Joi.number().integer().default(0),
+            limit: Joi.number().integer().default(10),
+            nome: Joi.string().min(3).max(50),
+          },
+        },
+      },
+      handler: (request, headers) => {
+        try {
+          const { skip, limit, nome } = request.query;
 
-                    const query = { nome: {$regex: `.*${nome}`}}
+          const query = { nome: { $regex: `.*${nome}` } };
 
-                    // console.log(typeof skip)
+          // console.log(typeof skip)
 
-                    return this.db.read(nome ? query : {}, parseInt(skip), parseInt(limit))
-                }catch(error){
-                    console.log('DEU RUIM', error)
-                    return 'ERRO'
-                }
-            }
+          return this.db.read(
+            nome ? query : {},
+            parseInt(skip),
+            parseInt(limit)
+          );
+        } catch (error) {
+          console.log("DEU RUIM", error);
+          return "ERRO";
         }
-    }
+      },
+    };
+  }
 
+  create() {
+    return {
+      path: "/herois",
+      method: "POST",
+      config: {
+        validate: {
+          failAction,
+          payload: {
+            nome: Joi.string().required().min(3).max(100),
+            poder: Joi.string().required().min(2).max(10),
+          },
+        },
+      },
+      handler: async (request) => {
+        try {
+          const { nome, poder } = request.payload;
+          const restult = await this.db.create({ nome, poder });
+          return { message: "Heroi cadastrado com sucesso!", _id: restult._id };
+        } catch (error) {
+          console.log("FAIL", error);
+          return "Internal Error!";
+        }
+      },
+    };
+  }
 }
 
-module.exports = HeroRoutes
+module.exports = HeroRoutes;
--- src/tests/apiHeroes.test.js
+++ src/tests/apiHeroes.test.js
@@ -1,56 +1,75 @@
-const assert = require('assert')
-const api = require('./../../api')
-let app = {}
+const assert = require("assert");
+const api = require("./../../api");
+let app = {};
+const MOCK_HEROIS_CADASTRAR = {
+  nome: "Test 1",
+  poder: "Testar",
+};
 
-describe.only('suite de testes da Api Heroes', function (){
-    this.beforeAll( async ()=>{
-        app = await api
-    })
+describe("suite de testes da Api Heroes", function () {
+  this.beforeAll(async () => {
+    app = await api;
+  });
 
-    it('listar herois', async () =>{
-        const result = await app.inject({
-            method: 'GET',
-            url: '/herois?skip=0&limit=10'
-        })
+  it("listar herois", async () => {
+    const result = await app.inject({
+      method: "GET",
+      url: "/herois?skip=0&limit=10",
+    });
 
-        const statusCode = result.statusCode
-        const dados = JSON.parse(result.payload)
+    const statusCode = result.statusCode;
+    const dados = JSON.parse(result.payload);
 
-        assert.deepEqual(statusCode, 200)
-        assert.ok(Array.isArray(dados))
-    })
+    assert.deepEqual(statusCode, 200);
+    assert.ok(Array.isArray(dados));
+  });
 
-    it('listar /herois - deve retornar um erro com limit incorreto', async()=>{
-        const TAMANHO_LIMITE = 'AEEEE'
-        const result = await app.inject({
-            method: 'GET',
-            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
-        })
+  it("listar /herois - deve retornar um erro com limit incorreto", async () => {
+    const TAMANHO_LIMITE = "AEEEE";
+    const result = await app.inject({
+      method: "GET",
+      url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`,
+    });
 
-        const errorResult = {
-            "statusCode": 400,
-            "error": "Bad Request",
-            "message": "child \"limit\" fails because [\"limit\" must be a number]",
-            "validation": {
-                "source": "query",
-                "keys": ["limit"]
-            }
-        }
+    const errorResult = {
+      statusCode: 400,
+      error: "Bad Request",
+      message: 'child "limit" fails because ["limit" must be a number]',
+      validation: {
+        source: "query",
+        keys: ["limit"],
+      },
+    };
 
-        assert.deepEqual(result.statusCode, 400)
-        assert.ok(result.payload, JSON.stringify(errorResult))
-    })
+    assert.deepEqual(result.statusCode, 400);
+    assert.ok(result.payload, JSON.stringify(errorResult));
+  });
 
-    it('listar /herois - deve listar um item', async()=>{
-        const NOME = 'Clone-3'
-        const result = await app.inject({
-            method: 'GET',
-            url: `/herois?skip=0&limit=100&nome=${NOME}`
-        })
+  it("listar  GET - /herois - deve listar um item", async () => {
+    const NOME = "Clone-3";
+    const result = await app.inject({
+      method: "GET",
+      url: `/herois?skip=0&limit=100&nome=${NOME}`,
+    });
 
-        const dados = JSON.parse(result.payload)
-        const statusCode = result.statusCode
-        assert.deepEqual(statusCode, 200)
-        assert.ok(dados[0].nome, NOME)
-    })
-})
+    const dados = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode, 200);
+    assert.ok(dados[0].nome, NOME);
+  });
+
+  it("cadastrar POST - /herois", async () => {
+    const result = await app.inject({
+      method: "POST",
+      url: `/herois`,
+      payload: MOCK_HEROIS_CADASTRAR,
+    });
+
+    const statusCode = result.statusCode;
+    const { message, _id } = JSON.parse(result.payload);
+
+    assert.ok(statusCode === 200);
+    assert.notStrictEqual(_id, undefined);
+    assert.deepEqual(message, "Heroi cadastrado com sucesso!");
+  });
+});
